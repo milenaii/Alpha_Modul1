1ffb16d732d63fcfe456c40984746aa6e2f9f9f4
Milena Ivanova <milena_ii@abv.bg>
2017-07-10 11:35:55 +0300

    Exceptions_Home

---
+++ Lect5_Exceptions/Exceptions/SquareRoot/StUpSquareRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class StUpSquareRoot
+    {
+        static void Main()
+        {
+            try
+            {
+                double n = double.Parse(Console.ReadLine());
+
+                if (n < 0)
+                {
+                    throw new FormatException();
+                }
+                double sqrtNum = Math.Sqrt(n);
+                Console.WriteLine("{0:0.000}",sqrtNum);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+
+        }
+    }
+}
