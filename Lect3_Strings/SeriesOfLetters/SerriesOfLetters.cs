a737df22f1811b2febeb38d58505e4380b352148
Milena Ivanova <milena_ii@abv.bg>
2017-07-17 12:26:04 +0300

    SeriesOfLetters

---
+++ Lect3_Strings/SeriesOfLetters/SerriesOfLetters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace SeriesOfLetters
+{
+    class SerriesOfLetters
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder output = new StringBuilder();
+            output.Append(input[0]);
+
+            for (int i = 1; i < input.Length ; i++)
+            {
+                if (input[i - 1] != input[i])
+                {
+                    output.Append(input[i]);
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
